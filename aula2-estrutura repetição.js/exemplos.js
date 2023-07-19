1a4f1fd8a18b4c65ce2ea93dd9ddf08b63ceb048
Gisele <giselesenai2022@gmail.com.br>
2023-07-18 22:02:58 -0300

    exercicios finalizados js

---
+++ aula2-estrutura repetição.js/exemplos.js
@@ -0,0 +1,62 @@
+// let nomes =["Alana", "Aléxia", "Samanta", "Thiago", "Odirlei"]
+
+// for (let contador = 0; contador < nomes.length; contador++) {
+//     const cadaElemento = nomes[contador];
+//     console.log(cadaElemento);
+    
+// }
+
+// nomes.forEach(cadaElemento => {
+//     console.log(cadaElemento);
+// })
+
+// // const e de constante, acrescenta mais segurança no sistema-  só pode ser lido
+// const frutas = ["Pera", "Uva", "Maça", "Salada Mista"]
+// for (let contador = 0; contador < frutas.length; contador++) {
+//     const cadaElemento = frutas[contador];
+//     console.log(contador, cadaElemento);
+    
+// }
+
+// frutas.forEach(cadaElemento => {
+//     console.log(cadaElemento);
+// })
+
+// if (salario < 1000) {
+//     let novosalario = salario + (salario * 0.1)
+//     console.log(' Nome: ' + nome + ' Cargo: ' + cargo + ' Salario: ' + novosalario)
+// }
+// else {
+//     console.log('Sucesso!!')
+
+// }
+
+// let salario = 1000
+
+// while (salario < 5000) {
+//     salario = salario + 100; //salario += 100;
+// contador++;
+// //     console.log('O seu salario ainda e R$: '+ salario);
+// console.log(contador);
+    
+// }
+
+// do{
+//   salario += 100;
+//   contador++;
+//   console.log('O seu salario ainda e R$: '+ salario);
+// } while (salario < 5000); 
+// console.log(contador);
+
+const prompt = require('prompt-sync')()
+
+let numeroEscolhido;
+
+do{
+numeroEscolhido = prompt ( 'Digite um numero: ');
+
+} while (numeroEscolhido != '5');
+     {
+    
+}
+
---
+++ aula2-estrutura repetição.js/exercicios.js
@@ -0,0 +1,115 @@
+// // Exercícios FOR
+
+//let prompt =require(`prompt-sync`)()
+
+// }
+
+// 1. Imprimir números de 1 a 10 utilizando o loop for
+// for (let contador = 0; contador <= 10; contador++) {
+//     console.log(contador);
+    
+
+
+// // 2.Imprimir números pares de 1 a 10.
+// for (let contador = 2; contador <= 10; contador +=2) {
+//     console.log(contador);
+    
+// }
+
+// // 3. Faça um programa que imprima os números ímpares de 0 a 50;
+// for (let contador = 1; contador < 50; contador +=2) {
+//     console.log(contador);
+    
+// 4. Faça um programa que imprima na tela a tabuada de um número
+// qualquer escolhido pelo usuário até o 10.
+
+
+// let multiplicador = parseInt(prompt("Digite um número a ser multiplicado: "));
+// for (let contador = 0; contador <= 10; contador++) {
+//     console.log(`${multiplicador*contador}`);
+// }
+
+// // 5. Imprimir os primeiros 10 números da sequência de Fibonacci.
+// 0 1 1 2 3 5 8 13
+
+// let numAnterior= 0;
+// let numAtual= 1
+
+// for (let contador = 0; contador < 10 ; contador++) {
+//     let proximoNUmero = numAnterior + numAtual;
+
+
+//     console.log(proximoNUmero);
+
+//     numAnterior = numAtual;
+//     numAtual = proximoNumero;
+
+// // 6. Imprimir números pares de 0 a 20 utilizando o loop while
+
+// let contador = 0
+// while (contador <= 20) {
+//     console.log(contador);
+//     contador +=2
+    
+// }
+
+    
+// // 7. Calcular a soma dos números de 1 a 100 utilizando a estrutura
+// // while.
+// 1 +2 + 3 + 4 + 5 + 6...100
+// 3 + 3
+// 6 + 4
+// 10 + 5
+// 15 + 6 
+
+// let contador =1
+// let resultado = 0
+    
+// while (contador <=100){
+//     resultado = resultado + contador;
+//     contador++;    
+//     }
+// console.log(resultado);
+
+// // 8. Faça um programa que receba a idade do usuário, enquanto a
+// // resposta do usuário for menor que 18 anos continue a solicitar a
+// // idade e mostre uma mensagem quando a idade do usuário for
+// // maior que 18.
+
+// 
+
+// let idade
+
+// do{
+//     idade = parseInt(prompt("Digite a sua idade: "))
+// }
+
+// while (idade < 18) {
+//     console.log("Ok! Sua idade é maior que 18");}
+
+// // 9. Faça um programa que receba um número do usuário e mostre
+// // no console a contagem regressiva até 0.
+// const prompt = require('prompt-sync')()
+
+// let numero = parseInt (prompt("Digite um número: "))
+
+// while (numero >= 0) {
+//     console.log(numero);
+//     numero --;
+    
+// }
+  
+
+// 10. Faça um programa que receba um número do usuário e continue
+// a pedir o número se o numero for diferente de 0.
+
+// const prompt= require('prompt-sync')()
+// let numero;
+
+// do {
+//     numero = prompt ("Digite um número: ")
+
+// }while (numero != 0);
+
+
+
